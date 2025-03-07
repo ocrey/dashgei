b419ae50a16229cb2704b3b0eeaed7184925a465
Ocrey <grmpeixoto@gmail.com>
2025-03-07 11:58:25 -0300

    ta dificil

--- main.js
+++ main.js
@@ -2,16 +2,21 @@ const ironhard = {
   offtank1: "Forretres",
   offtank2: "Klin Klang",
   t1h1: "Aegis Slash",
-  t1h2: "shiny Mawile",
+  t1h2: "Shiny Mawile",
   dps1: "Bronzong",
   dps2: "Metang",
-  dps3: "magneton",
-  tipagem: "Metal",
-  rotacao:  function(){
-    return ironhard.offtank1 + ", " + ironhard.offtank2 + ironhard.t1h1 + ", " + ironhard.t1h2;
-  }
+  dps3: "Magneton",
+  type: "Metal",
+
 };
 
-document.getElementById("teste").textContent = irohard.tipagem
 
+const myrotation = document.querySelector("myrotation");
+let uniqueId = ironhard.type;
+let counter = 1;
+while (document.getElementById(uniqueId)) {
+  uniqueId = ironhard.type + counter;
+  counter++;
+}
+myrotation.setAttribute("id", uniqueId);
 
