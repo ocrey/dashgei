2dec725470a5e6a9e3ccc0ff6b4a036e7f5f3e6a
Ocrey <grmpeixoto@gmail.com>
2025-03-27 17:09:58 -0300

    Testing use json for storage data

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-const ironhard = {
-  offtank1: "Forretres",
-  offtank2: "Klin Klang",
-  t1h1: "Aegis Slash",
-  t1h2: "Shiny Mawile",
-  dps1: "Bronzong",
-  dps2: "Metang",
-  dps3: "Magneton",
-  type: "Metal",
-
-};
-
+function HelloWorld() {
+    console.log("Hello, World!");
+}
 
 
 
